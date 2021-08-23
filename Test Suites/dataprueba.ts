807180d089f02e3a7042ae5df707d6d5ad5c2147
Ismelka <ismelkatg@gmail.com>
2021-08-23 11:11:54 -0400

    Datasuite

---
+++ Test Suites/dataprueba.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>dataprueba</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>a695ada7-c772-4f7d-8a86-e24e12354142</testSuiteGuid>
+</TestSuiteEntity>
